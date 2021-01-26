62b35abe79c3a130e3a0379b482b8ddf097e9d54
Jason <quietjason@gmail.com>
2021-01-26 21:37:58 +0800

    Add 5.longest_alindromic_substring.js.

---
+++ problems/5.longest_alindromic_substring.js
@@ -0,0 +1,54 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  let maxLength = 0, subStr = ''
+  const length = s.length
+  for (let i = 0; i < length; i++) {
+    for (let j = length - 1; j >= i; j--) {
+      if (s[i] === s[j]) {
+        let isPalindrome = true
+
+        for (let n = 1, max = Math.floor((j - i) / 2); n <= max; n++) {
+          if (s[i + n] !== s[j - n]) {
+            isPalindrome = false
+            break
+          }
+        }
+
+        if (isPalindrome) {
+          const subLength = j - i + 1
+          if (subLength > maxLength) {
+            maxLength = subLength
+            subStr = s.substring(i, j + 1)
+          }
+        }
+      }
+    }
+  }
+
+  return subStr
+}
+
+const assert = require('assert').strict
+
+let s, answer
+
+s = 'babad'
+answer = 'bab'
+assert.deepStrictEqual(longestPalindrome(s), answer)
+
+s = 'cbbd'
+answer = 'bb'
+assert.deepStrictEqual(longestPalindrome(s), answer)
+
+s = 'a'
+answer = 'a'
+assert.deepStrictEqual(longestPalindrome(s), answer)
+
+s = 'ac'
+answer = 'a'
+assert.deepStrictEqual(longestPalindrome(s), answer)
+
+console.log('Done')
