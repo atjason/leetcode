09eba35c55d1a847ccb5c4939851d49b61635a0e
Jason <quietjason@gmail.com>
2021-01-26 19:23:14 +0800

    Update 3.longest_substring.js

--- problems/3.longest_substring.js
+++ problems/3.longest_substring.js
@@ -5,6 +5,24 @@
  */
 var lengthOfLongestSubstring = function(s) {
   let maxLength = 0
+  let startIndex = 0
+  const map = {}
+
+  for (let i = 0, length = s.length; i < length; i++) {
+    const char = s[i]
+    if (map[char] !== undefined) {
+      startIndex = Math.max(startIndex, map[char] + 1)
+    }
+    
+    map[char] = i
+    maxLength = Math.max(maxLength, i - startIndex + 1)
+  }
+
+  return maxLength
+}
+
+var lengthOfLongestSubstring2 = function(s) {
+  let maxLength = 0
 
   const length = s.length
   for (let i = 0; i < length; i++) {
@@ -29,22 +47,26 @@ const assert = require('assert').strict
 
 let s, answer
 
-s = 'abcabcbb'
-answer = 3
-assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
+// s = 'abcabcbb'
+// answer = 3
+// assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
 
-s = 'bbbbb'
-answer = 1
-assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
+// s = 'bbbbb'
+// answer = 1
+// assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
 
-s = 'pwwkew'
-answer = 3
-assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
+// s = 'pwwkew'
+// answer = 3
+// assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
 
-s = ''
-answer = 0
-assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
+// s = ''
+// answer = 0
+// assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
 
-s = ' '
-answer = 1
+// s = ' '
+// answer = 1
+// assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
+
+s = 'abba'
+answer = 2
 assert.deepStrictEqual(lengthOfLongestSubstring(s), answer)
