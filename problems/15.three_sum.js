201b0c44204460691f1a456559f37316ab964031
Jason <quietjason@gmail.com>
2021-01-30 10:48:06 +0800

    Add 15.three_sum.js

--- problems/15.three_sum.js
+++ problems/15.three_sum.js
@@ -3,6 +3,36 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+  if (nums.length < 3) return []
+
+  nums = nums.sort((a, b) => a > b ? 1 : -1)
+  const length = nums.length
+  let result = []
+
+  for (let i = 0; i < length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue
+
+    let l = i + 1
+    let r = length - 1
+    while(l < r) {
+      const sum = nums[i] + nums[l] + nums[r]
+      if (sum < 0) {
+        l += 1
+      } else if (sum > 0) {
+        r -= 1
+      } else {
+        result.push([nums[i], nums[l], nums[r]])
+        do {
+          l += 1
+        } while(l < r && nums[l] === nums[l - 1])
+      }
+    }
+  }
+
+  return result
+}
+
+var threeSum2 = function(nums) {
   const fixedSum = 0
   nums = nums.sort((a, b) => a > b ? 1 : -1)
   const length = nums.length
